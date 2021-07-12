59d2a01d7bed49c4112680e0943d8ca05ccd1eff
Babita Singh <babita.singh@coolblue.nl>
2021-07-12 22:13:58 +0200

    added history functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
-import { Route } from "react-router-dom";
+import { Route, useHistory } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import ListItems from "./components/ListItems";
 import AddItems from "./components/AddItems";
 import { isTemplateSpan } from "typescript";
+import { BrowserRouter } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 function App() {
   const [items, setItems] = useState(["apple", "mangoes", "banana", "oranges"]);
+  const history = useHistory();
 
   function handleAdd(newitem: any) {
     console.log(newitem);
     //let newItems: any = items.push(newitem);
     setItems([...items, newitem]);
     console.log(items);
+    history.push("/");
   }
 
   function handleDelete(itemtoDelete: any) {
@@ -28,9 +32,14 @@ function App() {
         <h1>New React App</h1>
         <Route
           path="/"
+          exact
           render={() => <ListItems items={items} onDelete={handleDelete} />}
         />
-        <Route path="/Add" render={() => <AddItems onAdd={handleAdd} />} />
+        <Route
+          path="/Add"
+          exact
+          render={() => <AddItems onAdd={handleAdd} />}
+        />
       </header>
     </div>
   );
